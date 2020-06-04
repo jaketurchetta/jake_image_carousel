b261d96799d00630fa5b33885c5dc1a74ab72ac8
Jake Turchetta <14jturchetta@gmail.com>
2020-06-04 14:10:04 -0400

    add morgan, nodemon

--- Service/server/index.js
+++ Service/server/index.js
@@ -4,12 +4,14 @@ const cors = require('cors')
 const path = require('path');
 const bodyParser = require('body-parser');
 const expressStaticGzip = require('express-static-gzip')
+const morgan = require('morgan');
 
 const {getImages, addProperty, addPropertyImage, addPropertyImages, deleteProperty, deletePropertyImage, deletePropertyImages, updateImage} = require('./controller.js');
 const app = express();
 const PORT = 3003;
 
 // Middleware
+app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/', expressStaticGzip(path.join(__dirname, '../client/dist')))
